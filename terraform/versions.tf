2d2abbfec72a7e7cb0fdf4ef4ed9b35c0679c7e6
Kavya GM <kavya.gm@hitachivantara.com>
2026-02-12 17:55:42 +0530

    backend added

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -25,10 +25,10 @@ terraform {
   }
 
   # Uncomment and configure for remote state (recommended for team use)
-  # backend "azurerm" {
-  #   resource_group_name  = "tfstate-rg"
-  #   storage_account_name = "tfstateaccount"
-  #   container_name       = "tfstate"
-  #   key                  = "backstage-platform.terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatebackstagekavya"
+    container_name       = "tfstate"
+    key                  = "backstage-platform.terraform.tfstate"
+  }
 }
